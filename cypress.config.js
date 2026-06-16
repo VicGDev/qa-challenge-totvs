96b3953e8accbeb29a263d50314f2a11b81a6bfa
VicGDev <gomesvictora@gmail.com>
2026-06-16 01:35:45 -0300

    cypress structure

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,11 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    baseUrl: "https://automationexercise.com",
+
+    viewportWidth: 1366,
+    viewportHeight: 768,
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/register.cy.js
@@ -0,0 +1,5 @@
+describe('User Registration', () => {
+  it('should register a new user successfully', () => {
+    cy.visit('/')
+  })
+})
